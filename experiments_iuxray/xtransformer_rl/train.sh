a1e656d6f73a7f7ca4169b66da48bc41232a6edf
Pan Li <53412346+PLCarles@users.noreply.github.com>
2021-06-02 03:00:17 +1000

    Update train.sh

--- experiments_iuxray/xtransformer_rl/train.sh
+++ experiments_iuxray/xtransformer_rl/train.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0 python3 main.py --folder ./experiments_iuxray/xtransformer_rl --resume 39
+CUDA_VISIBLE_DEVICES=0 python3 main.py --folder ./experiments_iuxray/xtransformer_rl --resume 0
 
 # 39 is the epoch number of the pretrained model
