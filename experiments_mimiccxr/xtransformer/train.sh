c7aae9a3a92f15177e33a1ad4eb878f243b21c3c
Pan Li <53412346+PLCarles@users.noreply.github.com>
2021-06-02 01:54:15 +1000

    Update train.sh

--- experiments_mimiccxr/xtransformer/train.sh
+++ experiments_mimiccxr/xtransformer/train.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch \
   --nproc_per_node=1 main.py --folder ./experiments_mimiccxr/xtransformer \
   --dataset_name MIMICCXR \
-  --image_dir /content/mimic_cxr/images --ann_path /content/mimic_cxr/annotation.json
+  --image_dir /content/mimic_cxr/images --ann_path /content/mimic_cxr/annotation.json \
+  --submodel VSEGCN 
