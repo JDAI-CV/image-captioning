1e93c1da9642ea6a8b42d1aae0a149838abbc7a5
Pan Li <53412346+PLCarles@users.noreply.github.com>
2021-06-02 17:36:15 +1000

    Update train.sh

--- experiments_mimiccxr/xtransformer_rl/train.sh
+++ experiments_mimiccxr/xtransformer_rl/train.sh
@@ -1,5 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python3 main.py --folder ./experiments_mimiccxr/xtransformer_rl \
   --dataset_name MIMICCXR \
-  --image_dir /content/mimic_cxr/images --ann_path /content/mimic_cxr/annotation.json
+  --image_dir /content/mimic_cxr/images --ann_path /content/mimic_cxr/annotation.json \
+  --submodel VSEGCN 
 
 # 39 is the epoch number of the pretrained model
