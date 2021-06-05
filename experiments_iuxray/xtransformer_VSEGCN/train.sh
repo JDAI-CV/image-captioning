e2ab2396ba728f43e6e637d2adbbcd97f43303f8
Pan Li <53412346+PLCarles@users.noreply.github.com>
2021-06-05 14:44:23 +1000

    Update train.sh

--- experiments_iuxray/xtransformer_VSEGCN/train.sh
+++ experiments_iuxray/xtransformer_VSEGCN/train.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 main.py --folder ./experiments_iuxray/xtransformer_VSEGCN --submodel VSEGCN \
+CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 main.py --folder ./experiments_iuxray/xtransformer_VSEGCN 
+  --submodel VSEGCN \
+  --resume 0
+
+###if you want to use checkpoint, download your model in experiments_mimiccxr/xtransformer/snapshot and change 0 to your model's number
